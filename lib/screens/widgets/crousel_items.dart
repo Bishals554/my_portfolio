2f71e04b47d59930270a3aaead63bbb61c6ab09e
Bishal Shrestha <bishals554@gmaul.com>
2024-02-27 22:40:14 +0545

    first commit

---
+++ lib/screens/widgets/crousel_items.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_portfolio/models/crousel_items.dart';
+import 'package:my_portfolio/utilities/constants';
+
+List<CarouselItemModel> carouselItems = List.generate(
+  5,
+  (index) => CarouselItemModel(
+    text: Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            "F L U T T T E R  D E V E L O P E R",
+            style: GoogleFonts.oswald(
+              color: kPrimaryColor,
+              fontWeight: FontWeight.w900,
+              fontSize: 20.0,
+            ),
+          ),
+          SizedBox(
+            height: 18.0,
+          ),
+          Text(
+            "Bishal Shrestha",
+            style: GoogleFonts.oswald(
+              color: Colors.white,
+              fontSize: 40.0,
+              fontWeight: FontWeight.w900,
+              height: 1.3,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            "Flutter developer",
+            style: TextStyle(
+              color: kCaptionColor,
+              fontSize: 15.0,
+              height: 1.0,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            child: Wrap(
+              children: [
+                Text(
+                  "Need a mobile App?",
+                  style: TextStyle(
+                    color: kCaptionColor,
+                    fontSize: 15.0,
+                    height: 1.5,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: Text(
+                      " Got a project? Let's talk.",
+                      style: TextStyle(
+                        height: 1.5,
+                        color: Colors.white,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 25.0,
+          ),
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: Container(
+              decoration: BoxDecoration(
+                color: kPrimaryColor,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              height: 48.0,
+              padding: EdgeInsets.symmetric(
+                horizontal: 28.0,
+              ),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "GET STARTED",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 13.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    ),
+    image: Container(
+      child: Image.asset(
+        "assets/images/profile.png",
+        height: 500,
+        fit: BoxFit.contain,
+      ),
+    ),
+  ),
+);
